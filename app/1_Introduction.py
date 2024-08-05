ac8308f3642ae67fd817dba12fceb0e077a9a658
fahmizainal17 <fahmizainal9@gmail.com>
2024-08-05 17:59:20 +0800

    feat : add new chatbot feature to the page 1

--- app/1_Introduction.py
+++ app/1_Introduction.py
@@ -1,56 +1,67 @@
 import streamlit as st
+import replicate
+import os
+from module.Introduction.resume_text import resume_text
 from streamlit_chat import message
-import requests
 
-st.set_page_config(
-    page_title="Fahmi Zainal Chatbot",
-    page_icon=":robot:"
-)
+# Set up the page configuration
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot", page_icon=":robot:", layout="wide")
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-headers = {"Authorization": st.secrets['api_key']}
+# Define the app title and sidebar for API token input
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    
+    replicate_api = st.text_input('Enter Replicate API token:', type='password')
+    if replicate_api:
+        st.success('Proceed to entering your prompt message!', icon='👉')
+        os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    else:
+        st.warning('Please enter your credentials!', icon='⚠️')
 
-st.header("Fahmi Zainal Chatbot")
-st.markdown("[Github](https://github.com/fahmizainal17/FastAPI_ROAS_Dashboard)")
+    st.subheader('Models and parameters')
+    selected_model = st.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    if selected_model == 'Llama2-7B':
+        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+    elif selected_model == 'Llama2-13B':
+        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    temperature = st.slider('Temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.slider('Top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.slider('Max length', min_value=32, max_value=128, value=120, step=8)
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+# Initialize the session state to store messages
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+# Function to clear chat history
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+# Function for generating LLaMA2 response with resume context
+def generate_llama2_response(prompt_input):
+    context = resume_text + "\n\nUser: " + prompt_input + "\nAssistant:"
+    output = replicate.run(
+        llm,
+        input={"prompt": context, "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1}
+    )
+    output_list = list(output)  # Convert generator to list
+    return output_list[0]  # Assuming output is a list of strings
 
-def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
-    return input_text
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-# Resume text to be used for generating responses
-resume_text = """
-Hello! My name is Fahmi Zainal. I am a Full-Stack Data Scientist and AI enthusiast based in Selangor, Malaysia.
-I have expertise in Azure Databricks, Visual Studio Code, FastAPI, Streamlit, Docker, AWS Services, R programming, and Next.js.
-I work on developing machine learning models for analyzing human behaviors and socio-economic trends from survey data, and I manage interactive dashboards for detailed data analysis.
-I lead and mentor teams at INVOKE, manage interns and new hires, and have developed a Survey Automation platform using a diverse toolkit.
-Feel free to ask me about my work experience, skills, and projects!
-"""
+    # Generate response
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            st.write(response)
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
-user_input = get_text()
-
-if user_input:
-    output = query({
-        "inputs": {
-            "past_user_inputs": st.session_state.past,
-            "generated_responses": st.session_state.generated,
-            "text": f"{resume_text} {user_input}",
-        }, "parameters": {"repetition_penalty": 1.33},
-    })
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output["generated_text"])
-
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
